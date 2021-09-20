454c43e9af09d2db9f7afab59112b20477040915
ilnurvaleev <39234087+ilnurvaleev@users.noreply.github.com>
2021-09-20 15:04:09 +0300

    Update long_arithmetic.cpp

--- long_arithmetic.cpp
+++ long_arithmetic.cpp
@@ -41,7 +41,9 @@ struct long_arif {
     }
 
 
-
+    // b > a return 1
+    // b == a return 0
+    // b < a return -1
     int complong(long_arif b){
         if(b.a[0] > a[0])
             return 1;
@@ -69,18 +71,29 @@ struct long_arif {
         }
 
         while(c > 0){
-            a[0] = a[0]+1;
+            ++a[0];
             a[a[0]] = c % 10;
             c = c / 10;
         }
 
         while(a[a[0]] == 0 && a[0] > 1)
             --a[0];
-
-
     }
 
-    void minus(long_arif b){
+    void multiplication_long(long_arif b){
+        
+        for(int i = 1; i <= b.a[0]; ++i){
+            if(b.a[i] == 0)
+                continue;
+            for(int j = 1; j < i; ++j){
+                multiplication_int(10);
+            }
+            multiplication_int(b.a[i]);
+        }
+    }
+
+
+    void substruct(long_arif b){
 
         vector<int> mx, mn;
         if(complong(b) == 1){
@@ -115,6 +128,7 @@ struct long_arif {
         a = c;
     }
 
+
     void write(){
         if(znak)
             cout << "-";
@@ -124,19 +138,30 @@ struct long_arif {
 };
 
 
+
+// a / b and a > b
+int division(long_arif a, long_arif b){
+    int l = 0, r = 1001;
+    while(l + 1 < r){
+        int m = (l + r) / 2;
+        
+        long_arif c = b;
+        c.multiplication_int(m);
+        if(a.complong(c) == 1){
+            r = m;
+        }else{
+            l = m;
+        }
+    }
+    return l;
+}
+
 int main() {
-    int n;
-    cin >> n;
-    long_arif a;
-    a.init("1");
-
-
-    for(int i = 2; i <= n; i += 1)
-        a.multiplication_int(i);
-
-
-    a.write();
-
-
+    string s1, s2;
+    cin >> s1 >> s2;
+    long_arif a, b;
+    a.init(s1);
+    b.init(s2);
+    cout << division(a, b) << endl;
     return 0;
 }
